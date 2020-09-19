15b90fe4b530e9d81cb3a0bc21f19b96c6d4d4a9
Rajendra-Hande <raj.hande.net@gmail.com>
2020-09-19 20:06:33 +0530

    Consume Web API using Jquery AJAX
    Web API JQuery Ajax Implementation

---
+++ ConsumeAjax/ConsumeAjax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsumeAjax.MvcApplication" Language="C#" %>
---
+++ ConsumeAjax/ConsumeAjax/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ConsumeAjax
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
